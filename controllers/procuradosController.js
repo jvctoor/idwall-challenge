6bb09af7da7069b0c0b529ed21da511f299b54ec
João Victor <jvctor23@gmail.com>
2023-05-18 22:35:19 -0300

    1.3

--- controllers/procuradosController.js
+++ controllers/procuradosController.js
@@ -2,7 +2,7 @@ const db = require('../database.js')
 
 
 const getAllProcurados = (req, res) => {
-    db.query("SELECT p.ID, p.Nome, p.Genero, p.DataNascimento, p.LocalNascimento, JSON_ARRAYAGG(a.Acusacao) AS Acusacoes, SUM(a.severidade) AS SeveridadeTotal FROM Procurados p INNER JOIN Acusacoes a ON p.ID = a.ProcuradoID GROUP BY p.ID, p.Nome, p.Genero, p.DataNascimento, p.LocalNascimento ORDER BY SeveridadeTotal DESC;", (err, rows) => {
+    db.query("SELECT p.id, p.nome, p.genero, p.dataNascimento, p.localNascimento, JSON_ARRAYAGG(a.descricao) AS acusacoes, SUM(a.severidade) AS severidadeTotal, JSON_ARRAYAGG(JSON_OBJECT('descricao', e.descricao, 'dataEvidencia', e.dataEvidencia, 'imagens', e.imagens)) AS evidencias FROM Procurado p LEFT JOIN Acusacao a ON p.id = a.procurado_id LEFT JOIN Evidencia e ON a.id = e.acusacao_id GROUP BY p.id, p.nome, p.genero, p.dataNascimento, p.localNascimento ORDER BY severidadeTotal DESC;", (err, rows) => {
       res.send(rows)
     })
 }
@@ -11,16 +11,16 @@ const getProcuradoByNome = (req, res) => {
   const pesquisa = req.params.name;
   const porcentagemMinima = req.params.precisao;
   //console.log(pesquisa)
-  db.query("SELECT p.ID, p.Nome, p.Genero, p.DataNascimento, p.LocalNascimento, JSON_ARRAYAGG(a.Acusacao) AS Acusacoes, SUM(a.severidade) AS SeveridadeTotal FROM Procurados p INNER JOIN Acusacoes a ON p.ID = a.ProcuradoID GROUP BY p.ID, p.Nome, p.Genero, p.DataNascimento, p.LocalNascimento ORDER BY SeveridadeTotal DESC;", (err, resultados) => {
+  db.query("SELECT p.id, p.nome, p.genero, p.dataNascimento, p.localNascimento, JSON_ARRAYAGG(a.descricao) AS acusacoes, SUM(a.severidade) AS severidadeTotal, JSON_ARRAYAGG(JSON_OBJECT('descricao', e.descricao, 'dataEvidencia', e.dataEvidencia, 'imagens', e.imagens)) AS evidencias FROM Procurado p LEFT JOIN Acusacao a ON p.id = a.procurado_id LEFT JOIN Evidencia e ON a.id = e.acusacao_id GROUP BY p.id, p.nome, p.genero, p.dataNascimento, p.localNascimento ORDER BY severidadeTotal DESC;", (err, resultados) => {
     
     const resultadosFiltrados = resultados.filter(procurado => {
-      const nomeCompleto = procurado.Nome.toLowerCase();
+      const nomeCompleto = procurado.nome.toLowerCase();
       const nomesPesquisa = pesquisa.toLowerCase().split(' ');
       return nomesPesquisa.some(nome => nomeCompleto.includes(nome));
     });
 
     resultadosFiltrados.forEach(procurado => {
-      const nomeCompleto = procurado.Nome.toLowerCase();
+      const nomeCompleto = procurado.nome.toLowerCase();
       const nomesPesquisa = pesquisa.toLowerCase().split(' ');
       const totalNomesPesquisa = nomesPesquisa.length;
       let acertos = 0;
